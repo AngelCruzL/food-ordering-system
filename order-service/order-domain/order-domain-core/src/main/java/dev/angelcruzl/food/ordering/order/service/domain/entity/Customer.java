c1c12611965b039797c03a910a0eef1bbcdd08bb
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-12-25 21:45:36 -0600

    Add Customer & Restaurant entities

---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/angelcruzl/food/ordering/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package dev.angelcruzl.food.ordering.order.service.domain.entity;
+
+import dev.angelcruzl.food.ordering.domain.entity.AggregateRoot;
+import dev.angelcruzl.food.ordering.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
